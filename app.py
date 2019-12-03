23a31bf26ae44dab43ca328a13442c8ac03427f6
Yuto Mori <mt09forest.pc@gmail.com>
2019-12-03 23:21:02 +0900

    add: comment

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import urllib
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.figure import Figure
 import pandas as pd
-import datetime
 
 import os
 import glob
@@ -16,26 +15,34 @@ app = Flask(__name__)
 @app.route("/plot/<func>")
 def plot_graph(func='01'):
     fig = Figure()
-    ax = fig.add_subplot(111)
+    ax1 = fig.add_subplot(111)
+    # all.csv の 'id', 'ut', 'temp', 'hum' をDataFrame型に変換
     df = pd.read_csv('all.csv', usecols=['id', 'ut', 'temp', 'hum'])
 
     # ut(時刻)を基に昇順ソートする
     df.sort_values('ut', inplace=True)
 
+    # htmlで選択したuser_idのデータを抽出する
     df = df[df['id'] == int(func)]
     print(df)
-    ax.plot(df['ut'], df['hum'], color='C0', label='humidity')
-    ax.set_ylim([0,100])
-    ax.set_ylabel('humidity')
-    ax1 = ax.twinx()
-    
-    ax1.set_ylim([0, 40])
-    ax1.set_ylabel('temperature')
-    ax1.plot(df['ut'], df['temp'], color='C1', label='temperature')
+
+    # グラフに湿度をプロット
+    ax1.plot(df['ut'], df['hum'], color='C0', label='humidity')
+    ax1.set_ylim([0,100])
+    ax1.set_ylabel('humidity')
+
+    # グラフに温度をプロット
+    ax2 = ax1.twinx()  # ax1(湿度)と同じx軸を使う
+    ax2.set_ylim([0, 40])
+    ax2.set_ylabel('temperature')
+    ax2.plot(df['ut'], df['temp'], color='C1', label='temperature')
     
     fig.autofmt_xdate()
-
+    
+    # グラフ軸の設定
     fig.legend()
+
+    # 作成したグラフを画像に変換し、htmlで表示させる
     canvas = FigureCanvasAgg(fig)
     png_output = BytesIO()
     canvas.print_png(png_output)
@@ -56,13 +63,15 @@ def index():
     
     # python の list型 から pandas の DataFrame型 に変更
     df = pd.concat(list_df, sort=False)
+    print("merge csv")
     df.to_csv('./all.csv')
 
+    # Unixtime を pandas の datetime型に変換する
     df['ut'] = df['ut'].astype(int)
     df['ut'] = pd.to_datetime(df['ut'], unit="s")
     df['ut'] += pd.tseries.offsets.Hour(9) # 日本の時刻に合わせる
     df.to_csv('./all_datetime.csv')
-    print("merge csv")
+    
     return render_template("index.html", img_data=None)
 
 if __name__ == "__main__":
